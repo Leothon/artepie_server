37ae7359a60ab136ed5a340c2c8816ef528c6434
Leothon <langmancaizi@gmail.com>
2019-03-13 22:49:32 +0800

    网站上线

--- src/main/java/controller/GetDataController.java
+++ src/main/java/controller/GetDataController.java
@@ -5,6 +5,7 @@ import entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.GetDataService;
@@ -343,4 +344,8 @@ public class GetDataController {
     }
 
 
+    @GetMapping("/homepage")
+    public String visitHomePage(){
+        return "index";
+    }
 }
